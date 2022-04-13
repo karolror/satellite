7aed217e347288dff5bb2c7fe4aff69c81ca5eb2
Karol Rorat <karol@MBP-Karol.lan>
2022-04-13 19:38:11 +0200

    Code converted to work with Proporties + minor changes

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import StringProperty, DictProperty
+from kivy.properties import StringProperty, DictProperty, ObjectProperty
 import pandas as pd
+import xlsxwriter
 
 
 class MainWindow(Screen):
@@ -12,14 +13,6 @@ class MainWindow(Screen):
         self.ncolumns = None
         self.nrows_gene = None
         self.nrows_allel = None
-        self.af = None
-        self.gf = None
-        self.h_obs = None
-        self.h_exp = None
-        self.phobs = None
-        self.phexp = None
-        self.small_f = None
-        self.big_f = None
         super(Screen, self).__init__(**kwargs)
 
     def create_df(self):
@@ -30,6 +23,7 @@ class MainWindow(Screen):
         except:
             self.manager.get_screen('main').ids.hello_label.color = "red"
             self.manager.get_screen('main').ids.hello_label.text = "Error during loading file. Try again."
+            self.manager.get_screen('main').ids.results_button.disabled = True
             return False
 
     def get_basic(self):
@@ -63,9 +57,9 @@ class MainWindow(Screen):
     def get_hetero_exp(self):
         hetero_exp_dict = {}
         res_list = []
-        for key in self.af:
-            for a in self.af[key]:
-                res_list.append(self.af[key][a] ** 2)
+        for key in self.manager.af:
+            for a in self.manager.af[key]:
+                res_list.append(self.manager.af[key][a] ** 2)
             hetero_exp = 1 - sum(res_list)
             hetero_exp_dict[key] = hetero_exp
             res_list.clear()
@@ -91,11 +85,11 @@ class MainWindow(Screen):
     def get_hetero_obs(self):
         hetero_obs_dict = {}
         res_list = []
-        for key in self.gf:
-            for a in self.gf[key]:
+        for key in self.manager.gf:
+            for a in self.manager.gf[key]:
                 gene_check = a.split("/")
                 if gene_check[0] != gene_check[1]:
-                    res_list.append(self.gf[key][a])
+                    res_list.append(self.manager.gf[key][a])
                 hetero_obs = sum(res_list)
                 hetero_obs_dict[key] = hetero_obs
             res_list.clear()
@@ -105,66 +99,90 @@ class MainWindow(Screen):
         return sum(hetero.values()) / self.ncolumns
 
     def f_stats(self):
-        sub = [a - b for a, b in zip(self.h_exp.values(), self.h_obs.values())]
-        f_statistics = [a / b for a, b in zip(sub, self.h_exp.values())]
+        sub = [a - b for a, b in zip(self.manager.h_exp.values(), self.manager.h_obs.values())]
+        f_statistics = [a / b for a, b in zip(sub, self.manager.h_exp.values())]
         f_dict = dict(zip(self.col_names, f_statistics))
         return f_dict
 
     def f_statistic(self):
-        return (self.phexp - self.phobs) / self.phexp
+        return (self.manager.phexp - self.manager.phobs) / self.manager.phexp
 
     def calculate(self):
         if self.create_df():
             if self.get_basic():
-                self.af = self.get_allel_freq()
-                self.gf = self.get_gene_freq()
-                self.h_obs = self.get_hetero_obs()
-                self.h_exp = self.get_hetero_exp()
-                self.phobs = self.pop_hetero(self.h_obs)
-                self.phexp = self.pop_hetero(self.h_exp)
-                self.small_f = self.f_stats()
-                self.big_f = self.f_statistic()
-                #create proporties
-
-                # return text
-                self.manager.get_screen('main').ids.hello_label.color = "green"
-                self.manager.get_screen('main').ids.hello_label.text = "File calculated."
-                self.manager.get_screen('main').ids.results_button.disabled = False
-                #self.manager.get_screen('main').ids.hello_label.color = "red"
-                #self.manager.get_screen('main').ids.hello_label.text = "Something went wrong. Check your file and " \
-                                                                           #"try again"
+                try:
+                    self.manager.af = self.get_allel_freq()
+                    self.manager.gf = self.get_gene_freq()
+                    self.manager.h_obs = self.get_hetero_obs()
+                    self.manager.h_exp = self.get_hetero_exp()
+                    self.manager.phobs = self.pop_hetero(self.manager.h_obs)
+                    self.manager.phexp = self.pop_hetero(self.manager.h_exp)
+                    self.manager.small_f = self.f_stats()
+                    self.manager.big_f = self.f_statistic()
+                    # return communicate
+                    self.manager.get_screen('main').ids.hello_label.color = "green"
+                    self.manager.get_screen('main').ids.hello_label.text = "File calculated."
+                    self.manager.get_screen('main').ids.results_button.disabled = False
+                except:
+                    self.manager.get_screen('main').ids.hello_label.color = "red"
+                    self.manager.get_screen('main').ids.hello_label.text = "Something went wrong. Check your file and "\
+                                                                           "try again"
+                    self.manager.get_screen('main').ids.results_button.disabled = True
 
 
 class ResultsWindow(Screen):
 
     def to_str(self, thedict):
         converted = ""
-        for key in thedict:
-            for a in thedict[key]:
-                log_sum = str(key) + " | " + str(a) + " | " + str(round(thedict[key][a], 6)) + "\n"
+        try:
+            for key in thedict:
+                for a in thedict[key]:
+                    log_sum = str(key) + " | " + str(a) + " | " + str(round(thedict[key][a], 6)) + "\n"
+                    converted = converted + log_sum
+            return converted
+        except:
+            for k in thedict:
+                log_sum = str(k) + " | "  + str(round(thedict[k], 6)) + "\n"
                 converted = converted + log_sum
-        return converted
+            return converted
 
     def show_allel(self):
-        self.manager.allel_prop_str = self.to_str(self.manager.allel_prop)
-        self.manager.get_screen('results').ids.label_res.text = self.manager.allel_prop_str
+        self.manager.af_str = self.to_str(self.manager.af)
+        self.manager.get_screen('results').ids.label_res.text = self.manager.af_str
 
     def show_gene(self):
-        self.manager.gene_prop = self.to_str(self.gf)
-        self.manager.get_screen('results').ids.label_res.text = self.manager.gene_prop
+        self.manager.gf_str = self.to_str(self.manager.gf)
+        self.manager.get_screen('results').ids.label_res.text = self.manager.gf_str
 
     def show_hetero(self):
-        self.manager.hetero_prop = self.to_str(self.h_exp)
-        self.manager.get_screen('results').ids.label_res.text = self.manager.hetero_prop
+        self.manager.h_str = "*** Heterozigosity observed:\n" + self.to_str(self.manager.h_obs) \
+                             + "*** Heterozigosity expected:\n" + self.to_str(self.manager.h_exp) \
+                             + "*** Heterozigosity observed for population:\n" + str(self.manager.phobs) + "\n" \
+                             + "*** Heterozigosity expected for population:\n" + str(self.manager.phexp)
+        self.manager.get_screen('results').ids.label_res.text = self.manager.h_str
 
     def show_f(self):
-        self.manager.f_prop = self.to_str(self.big_f)
-        self.manager.get_screen('results').ids.label_res.text = self.manager.f_prop
+        self.manager.f_str = "*** F statistics:\n" + self.to_str(self.manager.small_f) + "*** Fst statistic:\n" \
+                             + str(round(self.manager.big_f, 6))
+        self.manager.get_screen('results').ids.label_res.text = self.manager.f_str
 
 
 class WindowManager(ScreenManager):
-    allel_prop = DictProperty(None)
-    allel_prop_str = StringProperty(None)
+    # Properties for calculations
+    af = DictProperty(None)
+    gf = DictProperty(None)
+    h_obs = DictProperty(None)
+    h_exp = DictProperty(None)
+    phobs = ObjectProperty(None)
+    phexp = ObjectProperty(None)
+    small_f = DictProperty(None)
+    big_f = ObjectProperty(None)
+    # Strings to show results
+    f_str = StringProperty(None)
+    af_str = StringProperty(None)
+    gf_str = StringProperty(None)
+    h_str = StringProperty(None)
+
 
 kv = Builder.load_file("my.kv")
 
